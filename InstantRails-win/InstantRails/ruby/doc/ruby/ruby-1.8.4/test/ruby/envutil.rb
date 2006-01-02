b8de643b95466dd45e9eb4188e0dced5c587b318
dgm <dgm@0df1b85c-4207-0410-bc56-cd30bc2d8147>
2006-01-02 17:16:51 +0000

    Adding ruby 1.8.4 from http://www.garbagecollect.jp/ruby/mswin32/en/ and extensions, except tcl/tk
    Still somewhat untested, but need this uploaded so I can revert easily. It *should* work. :)

---
+++ InstantRails-win/InstantRails/ruby/doc/ruby/ruby-1.8.4/test/ruby/envutil.rb
@@ -0,0 +1,24 @@
+module EnvUtil
+  def rubybin
+    if ruby = ENV["RUBY"]
+      return ruby
+    end
+    miniruby = "miniruby"
+    3.times do
+      if File.exist? miniruby or File.exist? miniruby+".exe"
+        return File.expand_path(miniruby)
+      end
+      miniruby = File.join("..", miniruby)
+    end
+    begin
+      require "rbconfig"
+      File.join(
+        Config::CONFIG["bindir"],
+	Config::CONFIG["ruby_install_name"] + Config::CONFIG["EXEEXT"]
+      )
+    rescue LoadError
+      "ruby"
+    end
+  end
+  module_function :rubybin
+end
