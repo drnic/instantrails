3b695ec61a2868907235dfd86ae6b2236e513d9a
curthibbs <curthibbs@0df1b85c-4207-0410-bc56-cd30bc2d8147>
2006-09-25 03:41:21 +0000

    Config path processing for RadRails

--- InstantRails-win/InstantRails-Mgr/GenConf.h
+++ InstantRails-win/InstantRails-Mgr/GenConf.h
@@ -3,3 +3,4 @@
 
 DWORD RegenerateConfFiles(HWND parent_window);
 DWORD GenerateConfFile(const char *szaPath, const char *szaTemplateFile, const char *szaDestFile, char caCommentChar);
+DWORD GenerateEscConfFile(const char *szaPath, const char *szaTemplateFile, const char *szaDestFile, char caCommentChar);
