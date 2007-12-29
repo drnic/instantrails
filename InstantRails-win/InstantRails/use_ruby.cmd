8ff3de2f4fd38d6fd5f5433deae33e509e10632b
rbazinet <rbazinet@0df1b85c-4207-0410-bc56-cd30bc2d8147>
2007-12-29 01:04:19 +0000

    Instant Rails Manager 2.0 Build updates

--- InstantRails-win/InstantRails/use_ruby.cmd
+++ InstantRails-win/InstantRails/use_ruby.cmd
@@ -3,9 +3,9 @@
 :
 : If you need to make changes to this file, you should edit
 : the source template file instead. The source template is
-: E:\Dev\RubyDev\InstantRails-Project\InstantRails-win\InstantRails\conf_files\use_ruby.cmd
+: D:\OpenSourceProjects\InstantRails\InstantRails-win\InstantRails\conf_files\use_ruby.cmd
 :-----------------------------------------------------------
-CD E:\Dev\RubyDev\InstantRails-Project\InstantRails-win\InstantRails
-PATH E:\Dev\RubyDev\InstantRails-Project\InstantRails-win\InstantRails\ruby\bin;E:\Dev\RubyDev\InstantRails-Project\InstantRails-win\InstantRails\mysql\bin;%PATH%
+CD D:\OpenSourceProjects\InstantRails\InstantRails-win\InstantRails
+PATH D:\OpenSourceProjects\InstantRails\InstantRails-win\InstantRails\ruby\bin;D:\OpenSourceProjects\InstantRails\InstantRails-win\InstantRails\mysql\bin;%PATH%
 cd rails_apps
 dir
